dd3cffc408d316bed771f054ae86d609bb66d8c2
maintth1807076 <maintth1807076@fpt.edu.vn>
2019-12-16 19:47:26 +0700

    hi

---
+++ daterangepickermvc/Controllers/DaterangePickerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace daterangepickermvc.Controllers
+{
+    public class DaterangePickerController : Controller
+    {
+        // GET: DaterangePicker
+        public ActionResult Index(DateTime? startDate, DateTime? endDate)
+        {
+            ViewBag.StartDate = startDate;
+            return View();
+        }
+    }
+}
